bd2ddbf84fc1435c2ad48bcda4e453386ad55d04
Shraman Gupta <worklife_sg@outlook.com>
2020-08-06 21:03:16 -0400

    Create JOINS.sql

---
+++ JOINS.sql
@@ -0,0 +1,33 @@
+----------------- Module: JOINS -------------------------
+
+/* allow us to combine information from multiple tables.
+* Alias claue - AS clause - allow to rename a column and functions as well
+* INNER, OUTER, LEFT, RIGHT, FULL, UNION JOINS
+*/
+
+------ AS clause ------
+-- Only used in SELECT statment
+
+SELECT COUNT(amount) AS num_transactions
+FROM payment; -- easy to read count of amount as number of transactions instead of just count
+
+--Actually showing how much customers have spent
+SELECT customer_id, SUM(amount)
+FROM payment
+GROUP BY customer_id; -- Not clear what sum is, what though process is?
+
+SELECT customer_id, SUM(amount) AS total_spent
+FROM payment
+GROUP BY customer_id; -- sum changed to total_spent; more clearer
+
+-- using HAVING function where mistkae can happen !
+SELECT customer_id, SUM(amount) AS total_spent
+FROM payment
+GROUP BY customer_id
+HAVING SUM(amount) >100; -- HAVING total_spent > 100 will give error as ALIAS 
+-- get assigned at the end at the data output, so not recognized by the system
+-- can't be used to filter out
+
+SELECT customer_id,amount AS new_name
+FROM payment
+WHERE amount > 2 -- same error if we use new_name instead of original column 'amount'
