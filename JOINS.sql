9c85b6a6f2939988e3040bca5a4f6678ed577bc4
Shraman Gupta <worklife_sg@outlook.com>
2020-08-12 11:10:51 -0400

    Update JOINS.sql

--- JOINS.sql
+++ JOINS.sql
@@ -20,7 +20,7 @@ SELECT customer_id, SUM(amount) AS total_spent
 FROM payment
 GROUP BY customer_id; -- sum changed to total_spent; more clearer
 
--- using HAVING function where mistkae can happen !
+-- using HAVING function where mistake can happen !
 SELECT customer_id, SUM(amount) AS total_spent
 FROM payment
 GROUP BY customer_id
@@ -46,3 +46,63 @@ ON payment.customer_id=customer.customer_id;
 SELECT payment_id,payment.customer_id,first_name FROM payment
 INNER JOIN customer
 ON payment.customer_id=customer.customer_id;
+
+-- FULL OUTER JOIN - allow us to specify how to deal with values only present in of the tables
+/* being joined. If there is no match, it returns null value for each table respectively.
+using WHERE after FULL OUTER JOIN allow us to filter the rows not found in both tables, which
+is exact opposite of INNER JOIN */
+
+-- TO check we don't have any customer information related to their payments. Means all payments
+-- are attached to the current customer ----- New privacy update
+
+SELECT * FROM  customer
+FULL  OUTER JOIN payment
+ON customer.customer_id=payment.customer_id -- fully join customer and payment tables
+WHERE customer.customer_id IS null -- information about customer that has never made payment
+OR payment.payment_id IS NULL; -- payment information associated with any customer
+-- filter to find rows either unique to customer table or unique to payment table
+-- Result: Null as there is no payment information linked with the customer or any info
+-- related to customer that has made any payment.
+
+--TO confirm above privacy update to further validate, but not true in all cases
+
+SELECT COUNT(DISTINCT customer_id) FROM payment; -- 599
+SELECT COUNT(DISTINCT customer_id) FROM customer; -- 599
+
+-- LEFT OUTER JOIN -
+/* set of records that are in the left table + if there is no  match with right table,
+the right table results in null for that non-matched value. WHERE - get rows unique to left table
+exclusive to only left table */
+
+-- film - left side - we will see rows just on film table and the one that matches with inventory table
+SELECT film.film_id, film.title, inventory_id,store_id
+FROM film
+LEFT OUTER JOIN inventory ON inventory.film_id=film.film_id
+WHERE inventory.film_id IS null; -- to filter the results that are not in inventory
+
+-- RIGHT OUTER JOIN - same as left outer join but here we have values to right table and 
+-- matched values with left table.
+
+---------- UNION ------------
+/* combine result set of two or more SELECT statements. concatenate or pasting two results together
+the idea to use UNION must be logical as results need to be stacked on top of each other.
+*/
+/* -----------------------Challenge Task-------------------------*/
+
+-- Situtation 1: California sales tax laws have changed and we need to alert our customers
+-- to this through email. What are the emails of the customers who live in california?
+
+SELECT address.district,customer.email FROM address
+INNER JOIN customer
+ON address.address_id=customer.address_id
+WHERE address.district = 'California';
+
+-- Situation 2: A customer walks in and is a huge fan of the actor 'Nick Wahlberg' and 
+-- wants to know which movies he is in. Get a list of all the movies he has been in?
+
+SELECT title, first_name, last_name FROM film_actor 
+INNER JOIN actor 
+ON film_actor.actor_id=actor.actor_id
+INNER JOIN film
+ON film_actor.film_id=film.film_id
+WHERE first_name='Nick' AND last_name='Wahlberg';
