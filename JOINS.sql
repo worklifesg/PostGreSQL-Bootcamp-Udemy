f146c61fa74c1b087bd24708d3f0be5d3d10e32c
Shraman Gupta <worklife_sg@outlook.com>
2020-08-12 15:17:25 -0400

    Update JOINS.sql

--- JOINS.sql
+++ JOINS.sql
@@ -106,3 +106,5 @@ ON film_actor.actor_id=actor.actor_id
 INNER JOIN film
 ON film_actor.film_id=film.film_id
 WHERE first_name='Nick' AND last_name='Wahlberg';
+
+----------------- Module ends -------------------------
