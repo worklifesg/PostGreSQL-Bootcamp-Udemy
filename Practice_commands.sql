0d319eec0ed342283fd49b0ca78b8ded17aaa673
Shraman Gupta <worklife_sg@outlook.com>
2021-07-28 14:39:09 -0400

    Update Practice_commands.sql

--- Practice_commands.sql
+++ Practice_commands.sql
@@ -49,3 +49,112 @@ SELECT
   COUNT(DISTINCT agency_name) as distinct_agency_name_records
 FROM
   `bigquery-public-data.new_york.311_service_requests`
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+/* To get distinct list of a certain column (like list of states only) in ascending order */
+
+SELECT
+  DISTINCT state
+FROM
+  `bigquery-public-data.usa_names.usa_1910_current`
+ORDER BY
+  state ASC /* DESC for descending order */
+
+/* Getting list of last 3 names */
+
+SELECT
+  DISTINCT name
+FROM
+  `bigquery-public-data.usa_names.usa_1910_current`
+ORDER BY
+  name DESC
+LIMIT
+  3
+
+/* Quiz 2 */
+
+/* 1. Consider the data set bigquery-public-data.new_york.citibike_trips , and the column tripduration which gives the length of the trip in seconds. Use distinct
+tripduration and ORDER BY to find how long the longest ride was in seconds.*/
+
+SELECT
+  DISTINCT tripduration
+FROM
+  `bigquery-public-data.new_york.citibike_trips`
+ORDER BY
+  tripduration DESC
+LIMIT
+  3
+
+/* 2. How many distinct birth_year values are there in the bigquery-public-data.new_york.citibike_trips data set?*/
+
+SELECT
+  COUNT(DISTINCT birth_year) 
+FROM
+  `bigquery-public-data.new_york.citibike_trips`
+
+/* 3. Use the DISTINCT and ORDER BY on the table bigquery-public-data.new_york.citibike_trips , to find the smallest (oldest) birth_year.*/
+
+SELECT
+  DISTINCT birth_year
+FROM
+  `bigquery-public-data.new_york.citibike_trips`
+ORDER BY
+  birth_year ASC
+LIMIT
+  3
+
+/* 4. Use DISTINCT and ORDERBY to find the largest (most recent) birth_year from the data set bigquery-public-data.new_york.citibike_trips .*/
+
+SELECT
+  DISTINCT birth_year
+FROM
+  `bigquery-public-data.new_york.citibike_trips`
+ORDER BY
+  birth_year DESC
+LIMIT
+  3
+
+/* 5. What are all the possible values from the column usertype in the dataset bigquery-public-data.new_york.citibike_trips?*/
+
+SELECT
+  DISTINCT usertype 
+FROM
+  `bigquery-public-data.new_york.citibike_trips`
+
+/* 6. What were the two dates of the most recent and oldest stoptimes in the dataset bigquery-public-data.new_york.citibike_trips ?*/
+
+SELECT
+  stoptime
+FROM
+  `bigquery-public-data.new_york.citibike_trips`
+ORDER BY
+  stoptime DESC
+LIMIT
+  1
+  
+/* 7. What are the distinct values from the column gender in the table bigquery-public-data.new_york.citibike_trips?*/
+
+SELECT
+  DISTINCT gender
+FROM
+  `bigquery-public-data.new_york.citibike_trips`
+ORDER BY
+  gender ASC
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+/* Distinct records and order for MULTIPLE columns */
+
+SELECT
+  DISTINCT borough,
+  major_category,
+  minor_category
+FROM
+  `bigquery-public-data.london_crime.crime_by_lsoa`
+ORDER BY
+  borough,
+  major_category,
+  minor_category
+  
