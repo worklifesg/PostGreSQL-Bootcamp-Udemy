c4b00e316fac715a5205213795ea4ffcb7d5e0cd
Shraman Gupta <worklife_sg@outlook.com>
2021-08-19 15:37:34 -0400

    Update case.sql

--- case.sql
+++ case.sql
@@ -31,3 +31,62 @@ FROM
   `bigquery-public-data.austin_bikeshare.bikeshare_trips`
 GROUP BY
   trip_duration
+
+----------------------------------------------------------------------------------------------------------------------------------------
+
+/* SIMPLE ONE CASE STATEMENT  - comes with null if not satisfied*/
+
+SELECT
+  CASE
+    WHEN LOWER(title) LIKE '%machine learning%' THEN 'machine_learning'
+END
+  AS topic_desc
+FROM
+  `bigquery-public-data.stackoverflow.stackoverflow_posts`
+LIMIT
+  100
+
+/* Replace null as Other */
+
+SELECT
+  CASE
+    WHEN LOWER(title) LIKE '%machine learning%' THEN 'machine_learning'
+  ELSE
+  'Other'
+END
+  AS topic_desc
+FROM
+  `bigquery-public-data.stackoverflow.stackoverflow_posts`
+LIMIT
+  100
+
+SELECT
+  CASE
+    WHEN LOWER(title) LIKE '%machine learning%' THEN 'machine_learning'
+    when lower(title) like '%python%' then 'python'
+    when lower(title) like '%big query%' then 'big query'
+  ELSE
+  'Other'
+END
+  AS topic_desc
+FROM
+  `bigquery-public-data.stackoverflow.stackoverflow_posts`
+LIMIT
+  100
+  
+/* To determine how many posts are there - groupby*/
+
+SELECT
+  CASE
+    WHEN LOWER(title) LIKE '%machine learning%' THEN 'machine_learning'
+    WHEN LOWER(title) LIKE '%python%' THEN 'python'
+    WHEN LOWER(title) LIKE '%big query%' THEN 'big query'
+  ELSE
+  'Other'
+END
+  AS topic_desc,
+  COUNT(*) AS num_topics
+FROM
+  `bigquery-public-data.stackoverflow.stackoverflow_posts`
+GROUP BY
+  topic_desc
